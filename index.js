4cbf8bdd3c4c58016def3189e3ef2604610b6968
NangunooriAbhinav <abhinavnangunoori.29@gmail.com>
2024-06-29 00:18:12 +0530

    merged testimonials

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Nav bar start
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
 var prevScrollpos = window.pageYOffset;
@@ -22,76 +22,105 @@ var navWrap = document.getElementById("navbar-wrap");
 var navBody = document.getElementById("navbar");
 
 window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos >= currentScrollPos) {
-        navWrap.style.top = "0";
-        navBody.style.boxShadow = "-1px 4px 15px 0px rgba(209, 205, 209, 0.5)";
-    } else {
-        navWrap.style.top = "-91px";
-        navBody.style.boxShadow = "-1px 4px 15px 0px rgba(209, 205, 209, 0)";
-    }
-    prevScrollpos = currentScrollPos;
-}
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos >= currentScrollPos) {
+    navWrap.style.top = "0";
+    navBody.style.boxShadow = "-1px 4px 15px 0px rgba(209, 205, 209, 0.5)";
+  } else {
+    navWrap.style.top = "-91px";
+    navBody.style.boxShadow = "-1px 4px 15px 0px rgba(209, 205, 209, 0)";
+  }
+  prevScrollpos = currentScrollPos;
+};
 // Nav bar end
-
 
 // Events Start
 var swiper = new Swiper(".mySwiper", {
-    breakpoints: {
-        1500: { slidesPerView: 3 },
-        900: { slidesPerView: 2, spaceBetween: 25 },
-    },
-    spaceBetween: 10,
-    slidesPerView: 1,
-    centeredSlides: false,
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
+  breakpoints: {
+    1500: { slidesPerView: 3 },
+    900: { slidesPerView: 2, spaceBetween: 25 },
+  },
+  spaceBetween: 10,
+  slidesPerView: 1,
+  centeredSlides: false,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 });
 // Events end
-
-
-
-
+var sw = new Swiper(".swiper-container", {
+  slidesPerView: 2,
+  spaceBetween: 40,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
 
 // Loading screen start
 const pageLoaded = () => {
-    // Animate on scroll
-    AOS.init(
-        {
-            duration: 800,
-            once: true
-        }
+  // Animate on scroll
+  AOS.init({
+    duration: 800,
+    once: true,
+  });
+
+  // Remove loader
+  const loader = document.getElementById("loader_block");
+  loader.style.opacity = "0";
+  setTimeout(() => (loader.style.display = "none"), 300);
+
+  // Lazy load images with cdn
+  const imgs = document.querySelectorAll("[data-src]");
+  imgs.forEach((img) => {
+    img.setAttribute(
+      "src",
+      "https://cdn.jsdelivr.net/gh/GfG-IIIT-Bh/GfG-IIIT-Bh.github.io" +
+        img.getAttribute("data-src").substring(1)
+    );
+  });
+
+  // Lazy load images without cdn
+  const imgs2 = document.querySelectorAll("[data-src-noncdn]");
+  imgs2.forEach((img) => {
+    img.setAttribute("src", img.getAttribute("data-src-noncdn"));
+  });
+
+  // Lazy load contact form iframe
+  document
+    .getElementById("contact-form")
+    .setAttribute(
+      "src",
+      "https://docs.google.com/forms/d/e/1FAIpQLSd8v5SA60CpZtwK2njAqfyT5b1FOwZhoqyGdhe2VNIXOXOEhg/viewform?embedded=true"
     );
 
-    // Remove loader
-    const loader = document.getElementById('loader_block');
-    loader.style.opacity = '0';
-    setTimeout(() => loader.style.display = 'none', 300);
-
-    // Lazy load images with cdn
-    const imgs = document.querySelectorAll('[data-src]');
-    imgs.forEach(img => {
-        img.setAttribute('src', 'https://cdn.jsdelivr.net/gh/GfG-IIIT-Bh/GfG-IIIT-Bh.github.io' + img.getAttribute('data-src').substring(1));
-    });
-
-    // Lazy load images without cdn
-    const imgs2 = document.querySelectorAll('[data-src-noncdn]');
-    imgs2.forEach(img => {
-        img.setAttribute('src', img.getAttribute('data-src-noncdn'));
-    });
-
-    // Lazy load contact form iframe
-    document.getElementById('contact-form').setAttribute('src', 'https://docs.google.com/forms/d/e/1FAIpQLSd8v5SA60CpZtwK2njAqfyT5b1FOwZhoqyGdhe2VNIXOXOEhg/viewform?embedded=true');
-
-    window.removeEventListener('load', pageLoaded);
-}
-window.addEventListener('load', pageLoaded);
+  window.removeEventListener("load", pageLoaded);
+};
+window.addEventListener("load", pageLoaded);
 // Loading screen end
